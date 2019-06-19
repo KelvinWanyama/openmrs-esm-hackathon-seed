b017a80047cfa88be5961f295067a7fc23ec7fc4
KelvinWanyama <kevmurumba@gmail.com>
2019-06-19 10:00:38 +0300

    Commented out the tests. Revist needed.

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Root from "./root.component";
 import { render } from "@testing-library/react";
 
-describe(`<Root />`, () => {
-  // it(`renders Root without dying`, () => {
-  //   render(<Root />);
-  // });
-});
+// describe(`<Root />`, () => {
+//   it(`renders Root without dying`, () => {
+//     render(<Root />);
+//   });
+// });
